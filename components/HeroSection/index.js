3d3f8bf74a4396bae5e8ad679538c644411f1e7e
rahulrajsbkk <rahulrajsbkk@gmail.com>
2021-12-17 17:55:26 +0530

    Hero Section Done

--- components/HeroSection/index.js
+++ components/HeroSection/index.js
@@ -4,12 +4,13 @@ import classNames from "./HeroSection.module.scss";
 import useWindowDimensions from "../../utils/WindowSize";
 
 function HeroSection() {
-  const { height } = useWindowDimensions();
+  const { width, height } = useWindowDimensions();
+  let base = Math.min(width, height);
   return (
     <section className={classNames.HeroSection} id="home">
       <div
         className={classNames.bgCircle}
-        style={{ height: height * 1.4, width: height * 1.4 }}
+        style={{ height: base * 1.4, width: base * 1.4 }}
       ></div>
       <div className={classNames.texts}>
         <div className={classNames.title}>
