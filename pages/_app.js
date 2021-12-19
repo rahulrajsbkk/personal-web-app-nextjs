7022dfa966c40b1de9eb8a0041ada9a52be4ce68
rahulrajsbkk <rahulrajsbkk@gmail.com>
2021-12-19 17:07:49 +0530

    Home Section Done

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,18 @@
+import { AnimatePresence } from "framer-motion";
 import "../styles/globals.scss";
 import "../styles/carousel.scss";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <AnimatePresence
+      exitBeforeEnter
+      initial={false}
+      onExitComplete={() => window.scrollTo(0, 0)}
+    >
+      <Component {...pageProps} />
+    </AnimatePresence>
+  );
 }
 
 export default MyApp;
